6475240049b0d5c998e19414bacbc8808bcc0ed3
Arber Shkurti <arbershkurti@yahoo.com>
2023-11-08 23:33:57 +0100

    step 1 bootstrap

---
+++ src/Components/HeroSections.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Container, Row, Col, Button } from "react-bootstrap";
+
+
+const HeroSections =() => {
+    return (
+        <Container className="hero-sections-container"></Container>
+    )
+}
+
+export default HeroSections;
