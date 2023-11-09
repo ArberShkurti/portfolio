80f0b87be5b1b61c80aa5052a26158752b9d54a7
Arber Shkurti <arbershkurti@yahoo.com>
2023-11-09 22:36:22 +0100

    first container

--- src/Components/HeroSections.jsx
+++ src/Components/HeroSections.jsx
@@ -4,7 +4,31 @@ import { Container, Row, Col, Button } from "react-bootstrap";
 
 const HeroSections =() => {
     return (
-        <Container className="hero-sections-container"></Container>
+        <Container className="hero-sections-container">
+            <Row className="h-100">
+
+                <Col xs md="8" className="d-flex flex-column justifiy-conten-center text-start px-4">
+                    <div className="mx-4">
+                        <h1> Photography Portofolio</h1>
+                        <p>
+                            Teks mbi foto, histori, pasion etj etj {""}
+                        </p>
+                    </div>
+                    <div className="bnts-wrapper">
+                        <a href="///">read my blog</a>
+                        <Button variant="sencondary">Learn more</Button>
+                    </div>
+                </Col>
+
+                <Col xs md="4">
+                    <div className="img-wrapper">
+
+                    </div>
+                </Col>
+
+
+            </Row>
+        </Container>
     )
 }
 
