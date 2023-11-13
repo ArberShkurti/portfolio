0a946e51165496839a92a48a89b55fa747577e87
Arber Shkurti <arbershkurti@yahoo.com>
2023-11-13 22:27:48 +0100

    header v2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.scss';
 import HeroSections  from '../src/Components/HeroSections';
 import { Container } from 'react-bootstrap';
+import Header from './Components/Header';
+import { useState } from 'react';
 
 function App() {
+  const [navbarOpen, setNavbarOpen] = useState(false);
   return (
     <div className="App">
       <Container>
+        <header navbarOpen={navbarOpen} setNavbarOpen={setNavbarOpen} />
         <HeroSections />
       </Container>
       
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
-const Header =() => {
+const Header =({navbarOpen, SetNavbarOpen}) => {
     return (
-        <div>Header</div>
+        <div>
+            <button className={navbarOpen ? 'menu-btn expandet': 'menu-btn'} onClick={()=> SetNavbarOpen((prevNavbarOpen) => !prevNavbarOpen)}>
+                <div className="menu-icon-container">
+                    <span className="icon-line "></span>
+                    <span className="icon-line "></span>
+                    <span className="icon-line "></span>
+                </div>
+            </button>
+        </div>
     )
 }
+
+
+export default Header ; 
