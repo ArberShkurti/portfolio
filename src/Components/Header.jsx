16d84c2156d8c6342ff068f3983e7d314b70d55e
Arber Shkurti <arbershkurti@yahoo.com>
2023-11-22 22:26:31 +0100

    menu btn

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Header =({navbarOpen, SetNavbarOpen}) => {
     return (
         <div>
-            <button className={navbarOpen ? 'menu-btn expandet': 'menu-btn'} onClick={()=> SetNavbarOpen((prevNavbarOpen) => !prevNavbarOpen)}>
+            <button className={navbarOpen ? 'menu-btn expanded': 'menu-btn'} onClick={()=> SetNavbarOpen((prevNavbarOpen) => !prevNavbarOpen)}>
                 <div className="menu-icon-container">
                     <span className="icon-line "></span>
                     <span className="icon-line "></span>
