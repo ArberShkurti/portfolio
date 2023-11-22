342922ce51882f6eb7fedc53c4775f7b14286b19
Arber Shkurti <arbershkurti@yahoo.com>
2023-11-22 22:44:30 +0100

    home

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.scss';
 import HeroSections  from '../src/Components/HeroSections';
 import { Container } from 'react-bootstrap';
 import Header from './Components/Header';
+import MenuOverlay from './Components/MenuOverlay';
 import { useState } from 'react';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
     <div className="App">
       <Container>
         <Header navbarOpen={navbarOpen} setNavbarOpen={setNavbarOpen} />
+        <MenuOverlay navbarOpen={navbarOpen} setNavbarOpen={setNavbarOpen}gi />
         <HeroSections />
       </Container>
       
---
+++ src/Components/MenuOverlay.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const MenuOverlay =({navbarOpen, SetNavbarOpen}) => {
+    return (
+        <nav className={`menu-container ${navbarOpen ? 'expanded' : ""}`}>
+            <ul className="menu-list">
+                <li className="nav-li">
+                    <a href="/" className="nav-link" onClick={() => SetNavbarOpen(false)}>Home</a>
+                </li>    
+                <li className="nav-li">
+                    <a href="/" className="nav-link" onClick={() => SetNavbarOpen(false)}>About</a>
+                </li>   
+                
+            </ul>    
+         </nav>
+    )
+}
+
+
+export default MenuOverlay ; 
