937246fc8140e79ca98a1dc0f64ce2524ebea8de
Arber Shkurti <arbershkurti@yahoo.com>
2023-12-13 22:14:21 +0100

    update app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+
 import './App.scss';
 import HeroSections from './Components/HeroSections';
-import { Container } from 'react-bootstrap';
+import { Col, Container, Row } from 'react-bootstrap';
 import Headerr from './Components/Headerr';
 import MenuOverlay from './Components/MenuOverlay';
 import { useState } from 'react';
 import ImagesCollections from './Components/ImagesCollections';
+import InformationSection from './Components/InformationSection';
+import FooterMenu from './Components/FooterMenu';
 import React from 'react';
 
 function App() {
@@ -15,7 +18,15 @@ function App() {
         <Headerr navbarOpen={navbarOpen} setNavbarOpen={setNavbarOpen} />
         <MenuOverlay navbarOpen={navbarOpen} setNavbarOpen={setNavbarOpen} />
         <HeroSections />
+        <InformationSection />
         <ImagesCollections />
+        <Container className='footer-section'>
+          <Row>
+            <Col>
+            <FooterMenu />
+            </Col>
+          </Row>
+        </Container>
       </Container>
       
     </div>
@@ -23,3 +34,34 @@ function App() {
 }
 
 export default App;
+
+
+
+
+
+
+// import './App.scss';
+// import HeroSections from './Components/HeroSections';
+// import { Container } from 'react-bootstrap';
+// import Headerr from './Components/Headerr';
+// import MenuOverlay from './Components/MenuOverlay';
+// import { useState } from 'react';
+// import ImagesCollections from './Components/ImagesCollections';
+// import React from 'react';
+
+// function App() {
+//   const [navbarOpen, setNavbarOpen] = useState(false);
+//   return (
+//     <div className="App">
+//       <Container>
+//         <Headerr navbarOpen={navbarOpen} setNavbarOpen={setNavbarOpen} />
+//         <MenuOverlay navbarOpen={navbarOpen} setNavbarOpen={setNavbarOpen} />
+//         <HeroSections />
+//         <ImagesCollections />
+//       </Container>
+      
+//     </div>
+//   );
+// }
+
+// export default App;
