fea6af494801b4ad3c12e081372fc17c65acdc66
Arber Shkurti <arbershkurti@yahoo.com>
2023-12-13 22:06:43 +0100

    view more

--- src/Components/ImagesCollections.jsx
+++ src/Components/ImagesCollections.jsx
@@ -3,14 +3,15 @@ import React from "react";
 
 
 const ImagesCollections = () => {
-    const imagesList = [...new Array(9)].map((number, index) => `./images/image-${index + 1}.jpg`);
+    const imagesList = [...new Array(8)].map((number, index) => `/images/image-${index + 1}.jpg`);
         
     
     return (
+        <>
         <ul className="images-collection-container">
             {imagesList.map((imgUrl, index) => {
                     return (
-                        <li className="item">
+                        <li className={index % 2 === 0 ? "item short" : "item tall"}>
                             <figure>
                                 <img src={imgUrl} alt="collection" />
                             </figure>
@@ -18,9 +19,51 @@ const ImagesCollections = () => {
                         );
                 })}
         </ul>
+        
+        <div className="d-flex mb-2">
+            <a href="/" className="mx-auto fs-3">
+                View more
+            </a>
+        </div>
+        </>
     );
     
 };
 
 export default ImagesCollections;
+
+
+
+
+
+
+
+
+
+
+// import React from "react";
+
+
+
+// const ImagesCollections = () => {
+//     const imagesList = [...new Array(9)].map((number, index) => `./images/image-${index + 1}.jpg`);
+        
+    
+//     return (
+//         <ul className="images-collection-container">
+//             {imagesList.map((imgUrl, index) => {
+//                     return (
+//                         <li className="item">
+//                             <figure>
+//                                 <img src={imgUrl} alt="collection" />
+//                             </figure>
+//                         </li>
+//                         );
+//                 })}
+//         </ul>
+//     );
+    
+// };
+
+// export default ImagesCollections;
 
