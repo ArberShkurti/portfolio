a705c0eb2e24b2b3faf3535fe14f8fa113808227
Arber Shkurti <arbershkurti@yahoo.com>
2023-12-19 22:00:30 +0100

    contact form

---
+++ src/ContactForm.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const ContactForm = () => {
+  return (
+    <div className='contact-form-section-container d-flex flex-column'>
+        <h1>
+            <span>Curiosity,</span>
+            <br></br>
+            piqued.
+        </h1>
+
+    </div>
+  )
+}
+
+export default ContactForm;
