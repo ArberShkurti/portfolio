b217d85bcde4dff8d29d80494853a4aafec1e7d7
Arber Shkurti <arbershkurti@yahoo.com>
2023-12-27 21:37:09 +0100

    delete..

--- src/App.js
+++ src/App.js
@@ -43,35 +43,3 @@ function App() {
 }
 
 export default App;
-
-
-
-
-
-
-// import './App.scss';
-// import HeroSections from './Components/HeroSections';
-// import { Container } from 'react-bootstrap';
-// import Headerr from './Components/Headerr';
-// import MenuOverlay from './Components/MenuOverlay';
-// import { useState } from 'react';
-// import ImagesCollections from './Components/ImagesCollections';
-// import React from 'react';
-
-// function App() {
-//   const [navbarOpen, setNavbarOpen] = useState(false);
-//   return (
-//     <div className="App">
-//       <Container>
-//         <Headerr navbarOpen={navbarOpen} setNavbarOpen={setNavbarOpen} />
-//         <MenuOverlay navbarOpen={navbarOpen} setNavbarOpen={setNavbarOpen} />
-//         <HeroSections />
-//         <ImagesCollections />
-//       </Container>
-      
-//     </div>
-//   );
-// }
-
-
-// export default App;
